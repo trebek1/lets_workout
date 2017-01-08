8012419abccc66cc6b2010ce7f3eb28b5f3747c3
Alex Trzeciak <trebek08@gmail.com>
2017-01-08 00:15:46 -0800

    Added login component and router for multiple views

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, IndexRoute, Route, browserHistory} from 'react-router';
+
+//components 
 import App from './App';
+import Login from './Login';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render((
+	<Router history={browserHistory}>
+		<Route path="/" pageId="home" component={App}>
+			<IndexRoute pageId="index" component={App}/>
+			<Route path="/login" pageId="Login" component={Login}/>
+		</Route>
+
+	</Router>), document.getElementById('root'));

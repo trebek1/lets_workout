e2c6afd489af1075824df303c304dbb014d91532
Alex Trzeciak <trebek08@gmail.com>
2017-01-08 17:19:16 -0800

    fixed header spacing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,19 +2,20 @@ import React, { Component } from 'react';
 
 export default class Header extends Component {
   	
-  	
+
 
   	render() {
     return (
     	<div>	
 	        <div className="header">
 	      		<div className="title">Let&apos;s Workout!</div>
-	      		<ul className="nav">
-	      	 	<li className="item"><a href="#">Login</a></li>
-	      	 	<li className="item"><a className=" selected" href="#">Home</a></li>
-	      	 	<li className="item"><a href="#">Log</a></li>
-	      	 	<li className="item"><a href="#">Compare</a></li>
-	      	 </ul>
+	      		<ul className="nav clearfix">
+		      	 	<li className="item"><a href="#">Login</a></li>
+		      	 	<li className="item"><a href="#">Sign Up</a></li>
+		      	 	<li className="item"><a className=" selected" href="#">Home</a></li>
+		      	 	<li className="item"><a href="#">Log</a></li>
+		      	 	<li className="item"><a href="#">Compare</a></li>
+	      	 	</ul>
 	      	 </div>
 	      	 
       	</div>

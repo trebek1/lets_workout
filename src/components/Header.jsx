2331c967d12786a9b05c159043925a716defed32
Alex Trzeciak <trebek08@gmail.com>
2017-01-08 17:09:13 -0800

    added small menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 
 export default class Header extends Component {
+  	
+  	
+
   	render() {
-    return (	
-        <div className="header">
-      		Let&apos;s Workout!
-      	 </div>
+    return (
+    	<div>	
+	        <div className="header">
+	      		<div className="title">Let&apos;s Workout!</div>
+	      		<ul className="nav">
+	      	 	<li className="item"><a href="#">Login</a></li>
+	      	 	<li className="item"><a className=" selected" href="#">Home</a></li>
+	      	 	<li className="item"><a href="#">Log</a></li>
+	      	 	<li className="item"><a href="#">Compare</a></li>
+	      	 </ul>
+	      	 </div>
+	      	 
+      	</div>
+
     );
   }
 }

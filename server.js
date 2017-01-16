a12b0ffaac257b16a6c13ef87e3018b1dd5743f4
Alex Trzeciak <trebek08@gmail.com>
2017-01-15 17:59:29 -0800

    did some linting

--- server.js
+++ server.js
@@ -42,68 +42,15 @@ app.use("/", function (req, res, next) {
   next(); 
 });
 
-
-
-// db.User.createSecure("bill", "foobar", function(err, user){
-// 	console.log("success!", user);
-// 	var id = user._id; 	
-// 	console.log("this is id ", id); 
-// 	db.Day.addDay('1/1/2015', 205, 1,1,1,"none","good food",id, function(err,day){
-// 		console.log("day added ", day, err); 
-// 	});	
-
-
-// });
- //(date, weight, alcohol,coffee,miles,workoutNotes,foodNotes, cb) {
-
-
-
-//var MongoClient = require('mongodb').MongoClient
-
-// Connection URL
-//var url = 'mongodb://localhost:27017/workout';
-
-// Use connect method to connect to the server
-// MongoClient.connect(url, function(err, db) {
-  
-//   console.log("Connected successfully to server");
-//   var myCursor = db.collection('users').find({}); 
-//   	while (myCursor.hasNext()) {
-//    		print(tojson(myCursor.next()));
-// }
-//   console.log(workout)
-
-//   db.close();
-// });
-
-
 app.use(require('webpack-dev-middleware')(compiler, {
   publicPath: config.output.publicPath
 }));
 
 app.use(require('webpack-hot-middleware')(compiler));
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(bodyParser.json())
-
-// app.use('*', function(req,res){ 
-// 	res.header("Access-Control-Allow-Origin", "*"); 
-// 	res.header("Access-Control-Allow-Credentials", "true"); 
-// 	res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT"); 
-// 	res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"); 
-// });
-
-app.get('/api', function(req, res){
-	res.json({
-		'alex': {
-			'was': 'here'
-		},
-		'ian': {
-			'is' : 'developer'
-		}
-	});
-});
+app.use(bodyParser.json());
 
 app.get('/session', function(req, res){
 	req.currentUser(function(err, user){
@@ -113,17 +60,14 @@ app.get('/session', function(req, res){
 }); 
 
 app.post('/signup', function(req, res){
-	
 	var info = req.body; 
 	db.User.find({email: info.username}, function(err,user){
-		
 		if(user.length === 0){
 			db.User.createSecure(info.username, info.password, function(err, user){
 			});	
 		}else{
 			console.log("already in database");
 		}
-		
 	});
 });
 
@@ -133,17 +77,12 @@ app.post('/login', function(req, res){
 	db.User.authenticate(user.username, user.password, function(err, user){
 		req.login(user);
 		res.send('logged in!');
-
 	});
-	
-	
 });
 
 app.get('/logout', function(req, res){
 	req.logout(); 
-
 	res.send('logout finished');
-
 });
 
 
@@ -156,6 +95,5 @@ app.listen(3000, function(err) {
   if (err) {
     return console.error(err);
   }
-
   console.log('Listening at http://localhost:3000/');
 })

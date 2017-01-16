7267d9a3a8c1afc76d638d06585e75cee0a751f4
Alex Trzeciak <trebek08@gmail.com>
2017-01-16 00:06:04 -0800

    code cleanup

--- server.js
+++ server.js
@@ -41,14 +41,9 @@ app.use("/", function (req, res, next) {
   next(); 
 });
 
-app.use(require('webpack-dev-middleware')(compiler, {
-  publicPath: config.output.publicPath
-}));
-
+app.use(require('webpack-dev-middleware')(compiler, {publicPath: config.output.publicPath}));
 app.use(require('webpack-hot-middleware')(compiler));
-
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(bodyParser.json());
 
 app.get('/session', function(req, res){

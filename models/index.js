33d8e4bb4121136fd4fb7fda1690fb5543bd6d10
Alex Trzeciak <trebek08@gmail.com>
2017-01-20 00:09:26 -0800

    prep for production

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 // models/index.js
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/workout");
+//mongoose.connect("mongodb://localhost:27017/workout");
+var url = process.env.MONGOLAB_URI;
+mongoose.connect(url);
 
 module.exports.User = require("./User");
 module.exports.Day = require("./Day");
